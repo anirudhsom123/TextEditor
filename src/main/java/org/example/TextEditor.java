d92c2949544af252866f596997bc0a11ed141736
anirudhsom123 <anirudhsom007@gmail.com>
2025-09-16 22:33:48 +0530

    adding save file functionality

--- src/main/java/org/example/TextEditor.java
+++ src/main/java/org/example/TextEditor.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.nio.Buffer;
 
 
 public class TextEditor implements ActionListener {
@@ -14,33 +15,33 @@ public class TextEditor implements ActionListener {
     JMenu file, edit;
 
     // File menu items
-    JMenuItem newFile,openFile,saveFile;
+    JMenuItem newFile, openFile, saveFile;
 
     // Edit menu items
-    JMenuItem cut, copy , paste , selectAll , close;
+    JMenuItem cut, copy, paste, selectAll, close;
 
     // text area
     JTextArea textArea;
 
-    TextEditor(){
+    TextEditor() {
 
         //Initialize a frame
-        frame=new JFrame();
+        frame = new JFrame();
 
         //Initialize menubar
-        menuBar=new JMenuBar();
+        menuBar = new JMenuBar();
 
         //Initialize text area
-        textArea=new JTextArea();
+        textArea = new JTextArea();
 
         // Initialize Menus
-        file=new JMenu("File");
-        edit=new JMenu("Edit");
+        file = new JMenu("File");
+        edit = new JMenu("Edit");
 
         // Initialize file menu items
-        newFile =new JMenuItem("New Window");
-        openFile=new JMenuItem("OPen File");
-        saveFile=new JMenuItem("Save File");
+        newFile = new JMenuItem("New Window");
+        openFile = new JMenuItem("Open File");
+        saveFile = new JMenuItem("Save File");
 
         // Add action Listener to file menu items
         newFile.addActionListener(this);
@@ -53,11 +54,11 @@ public class TextEditor implements ActionListener {
         file.add(saveFile);
 
         // Initialize edit menu items
-        cut=new JMenuItem("Cut");
-        copy=new JMenuItem("Copy");
-        paste=new JMenuItem("Paste");
-        selectAll=new JMenuItem("Select All");
-        close=new JMenuItem("Close");
+        cut = new JMenuItem("Cut");
+        copy = new JMenuItem("Copy");
+        paste = new JMenuItem("Paste");
+        selectAll = new JMenuItem("Select All");
+        close = new JMenuItem("Close");
 
         // Adding action Listeners to edit menu items
         cut.addActionListener(this);
@@ -85,83 +86,118 @@ public class TextEditor implements ActionListener {
         frame.add(textArea);
 
         // Set dimensions of frame
-        frame.setBounds(0,0,400,400);
+        frame.setBounds(0, 0, 400, 400);
         frame.setVisible(true);
         frame.setLayout(null);
     }
 
     @Override
-    public void actionPerformed(ActionEvent actionEvent){
+    public void actionPerformed(ActionEvent actionEvent) {
 
-        if(actionEvent.getSource()==cut){
+        if (actionEvent.getSource() == cut) {
             //perform cut operation
             textArea.cut();
         }
-        if(actionEvent.getSource()==copy){
+        if (actionEvent.getSource() == copy) {
             //perform copy operation
             textArea.copy();
         }
 
-        if(actionEvent.getSource()==paste){
+        if (actionEvent.getSource() == paste) {
             //perform paste operation
             textArea.paste();
         }
 
-        if(actionEvent.getSource()==selectAll){
+        if (actionEvent.getSource() == selectAll) {
 
             //perform Select all Operation
             textArea.selectAll();
         }
 
-        if(actionEvent.getSource()==close){
+        if (actionEvent.getSource() == close) {
             //perform close editor Operation
             System.exit(0);
         }
 
-        if(actionEvent.getSource()==openFile){
+        if (actionEvent.getSource() == openFile) {
 
             //Open File chooser
-            JFileChooser fileChooser=new JFileChooser("users/");
+            JFileChooser fileChooser = new JFileChooser("users/");
 
             int chooseOption = fileChooser.showOpenDialog(null);
 
-            // if we have choosen Open Button
-            if(chooseOption==JFileChooser.APPROVE_OPTION){
+            // if we have choose Open Button
+            if (chooseOption == JFileChooser.APPROVE_OPTION) {
 
                 // Getting the selected file
-                File file= fileChooser.getSelectedFile();
+                File file = fileChooser.getSelectedFile();
 
                 //get path of file
-                String filePath= file.getPath();
+                String filePath = file.getPath();
 
-                try{
+                try {
 
                     // initialize file reader
-                    FileReader fileReader=new FileReader(filePath);
+                    FileReader fileReader = new FileReader(filePath);
 
                     // Initialize Buffer Reader
-                    BufferedReader bufferedReader=new BufferedReader(fileReader);
-                    String intermediate="", output="";
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+                    String intermediate = "", output = "";
 
                     // Read content line by line
-                    while((intermediate = bufferedReader.readLine())!=null){
-                        output+=intermediate+"\n";
+                    while ((intermediate = bufferedReader.readLine()) != null) {
+                        output += intermediate + "\n";
                     }
 
                     // Set the output string to text area
                     textArea.setText(output);
 
-                } catch(IOException fileNotFoundException){
+                } catch (IOException fileNotFoundException) {
                     fileNotFoundException.printStackTrace();
 
                 }
 
             }
+
+        }
+
+        // if we chose save file button
+        if (actionEvent.getSource() == saveFile) {
+
+            // Initialize file picker
+            JFileChooser fileChooser1 = new JFileChooser();
+
+            //get choose option from file chooser
+            int chooseOption1 = fileChooser1.showSaveDialog(null);
+
+            //Check we clicked on save button
+            if (chooseOption1 == JFileChooser.APPROVE_OPTION) {
+
+                // choose a new file with chosen directory path and file name
+                File file = new File(fileChooser1.getSelectedFile().getAbsolutePath() + ".txt");
+
+                try {
+                    //Initialize file writer
+                    FileWriter fileWriter = new FileWriter(file);
+
+                    //buffer writer
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                    // write the context of the text area to the file
+                    textArea.write(bufferedWriter);
+                    bufferedWriter.close();
+
+                } catch (IOException ioException) {
+
+                    ioException.getStackTrace();
+                }
+            }
         }
     }
+
     public static void main(String[] args) {
 
-        TextEditor textEditor=new TextEditor();
+        TextEditor textEditor = new TextEditor();
 
     }
 }
